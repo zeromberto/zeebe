4d098d5ea2ef93eb5046aaec1d01bb90c8c9664c
pihme <pihme@users.noreply.github.com>
2020-05-06 17:09:26 +0200

    do not merge - add flaky test

---
+++ util/src/test/java/io/zeebe/util/FlakyTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.0. You may not use this file
+ * except in compliance with the Zeebe Community License 1.0.
+ */
+package io.zeebe.util;
+
+import java.util.Random;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FlakyTest {
+
+  Random random = new Random();
+
+  @Test
+  public void shouldFailNineInTenInvocations() {
+    if (random.nextDouble() < 0.9) {
+      Assert.fail("failed");
+    }
+  }
+
+  @Test
+  public void shouldFailOnceInTwoInvocations() {
+    if (random.nextDouble() < 0.5) {
+      Assert.fail("failed");
+    }
+  }
+
+  @Test
+  public void shouldFailOnceInTenInvocations() {
+    if (random.nextDouble() < 0.1) {
+      Assert.fail("failed");
+    }
+  }
+
+  @Test
+  public void shouldFailOnceInHundredInvocations() {
+    if (random.nextDouble() < 0.01) {
+      Assert.fail("failed");
+    }
+  }
+}
