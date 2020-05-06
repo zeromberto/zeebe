93fedad5a761e4ecb2ca6e1a80103899ef492ff5
pihme <pihme@users.noreply.github.com>
2020-05-06 18:17:59 +0200

    fixup: diambiguate file name

--- .ci/scripts/distribution/test-java.sh
+++ .ci/scripts/distribution/test-java.sh
@@ -3,14 +3,14 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-mvn -o -B -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -Dzeebe.it.skip -DtestMavenId=1 -Dsurefire.rerunFailingTestsCount=5 | tee test.txt
+mvn -o -B -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -Dzeebe.it.skip -DtestMavenId=1 -Dsurefire.rerunFailingTestsCount=5 | tee test-java.txt
 
-if grep -q "\[WARNING\] Flakes:" test.txt; then
-  grep "\[ERROR\]   Run 1: " test.txt | awk '{print $4}' >> ./target/FlakyTests.txt
+if grep -q "\[WARNING\] Flakes:" test-java.txt; then
+  grep "\[ERROR\]   Run 1: " test-java.txt | awk '{print $4}' >> ./target/FlakyTests.txt
 
   echo ERROR: Flaky Tests detected>&2
-  rm test.txt
+  rm test-java.txt
   exit 1
 fi
 
-rm test.txt
+rm test-java.txt
